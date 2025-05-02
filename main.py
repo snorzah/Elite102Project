d334037607197bd4f6d0cca1711ef9e43ecc6488
Hamzah <hamzahraoof@gmail.com>
2025-05-01 21:09:35 -0500

    Added comments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from sqlConnection import *
+from sqlConnection import * # Import the methods
+# The Main menu function, where everything else branches from
 def mainMenu():
     hasAccount = input("Welcome to ___ Banking!\nDo you have an Account?(yes/no) ")
     if (hasAccount == "yes"):
@@ -6,51 +7,64 @@ def mainMenu():
     elif (hasAccount == "no"):
         accountCreationMenu()
 
+# The Menu for people who already have an account
 def withAccountMenu():
     userIn = input("Please Enter an Action:\n1: Update Email\n2: Update Password\n3: Deposit\n4: Withdraw \n5: Display Balance\n6: Delete Account\n")
     if (userIn == "1"):
+        # Input the user's necessary info to execute the updateEmail method
         oldEmail = input("Please Enter Your Old Email: ")
         newEmail = input("Please Enter Your New Email: ")
         password1 = input("Please Enter Your Password: ")
         updateEmail(oldEmail, newEmail, password1)
         print("Email Changed Successfully!")
     elif (userIn == "2"):
+        # Input the user's necessary info to execute the updatePassword method
         email = input("Please Enter Your Email: ")
         oldPassword = input("Please Enter Your Old Password: ")
         newPassword = input("Please Enter Your New Password: ")
         updatePassword(email, oldPassword, newPassword)
     elif (userIn == "3"):
+        # Input the user's necessary info to execute the addBalance method
         email = input("Please Enter Your Email: ")
         password = input("Please Enter Your Password: ")
         amount = float(input("Please Enter Your Deposit Amount: "))
         addBalance(email, password, amount)
     elif (userIn == "4"):
+        # Input the user's necessary info to execute the withdrawBalance method
         email = input("Please Enter Your Email: ")
         password = input("Please Enter Your Password: ")
         amount = float(input("Please Enter Your Withdraw Amount: "))
         withdrawBalance(email, password, amount)
     elif (userIn == "5"):
+        # Input the user's necessary info to execute the displayBalance method
         email = input("Please Enter Your Email: ")
         password = input("Please Enter Your Password: ")
         displayBalance(email, password)
     elif (userIn == "6"):
+        # Confirm to see if the user really wants to delete their account
         confirmation = input("Are You Sure You Would Like to do This? (yes/no)")
         if (confirmation == "yes"):
+            # If they confirm, input their details and delete their account
             email = input("Please Enter Your Email: ")
             password = input("Please Enter Your Password: ")
             deleteAccount(email, password)
         elif (confirmation == "no"):
+            # Otherwise, return to main menu
             print("Okay! Returning to Main Menu...\n")
 
+# The menu for people who don't have an account and to create an account
 def accountCreationMenu():
     print("Welcome to the Account Creation Portal!")
+    # Input the user's information to create an account using the makeAccount method
     email = input("Please Input Your Email: ")
     password = input("Please Enter a Password: ")
     startingBalance = float(input("Please Enter a Starting Balance: "))
     makeAccount(email, password, startingBalance)
 
+# Test case to see if the functions work
 if __name__ == "__main__":
     wantsToContinue = True
+    # Admin password to stop the ATM from running when necessary
     adminPassword = "123asd123asd"
     while True:
         mainMenu()
--- sqlConnection.py
+++ sqlConnection.py
@@ -1,48 +1,57 @@
 import mysql.connector
 
+# Making an account to the database based on email, password, and initial balance
 def makeAccount(email, password, balance):
     connection = mysql.connector.connect(host = 'localhost', user = 'root', database = 'bankingdata', password = 'haur@b0t')
     query = (f"INSERT INTO banking (email, password, balance) VALUES ('{email}', '{password}', {balance})")
     cursor = connection.cursor()
     cursor.execute(query)
-
+    # Add and close the connection to SQL
     connection.commit()
     cursor.close()
     connection.close()
 
+# Deleting an account based on email. Requires password (self explanatory: for safety reasons)
 def deleteAccount(email, password):
     connection = mysql.connector.connect(host = 'localhost', user = 'root', database = 'bankingdata', password = 'haur@b0t')
     query = (f"DELETE FROM banking WHERE email='{email}' AND password='{password}'")
+    # Create a cursor and execute the command
     cursor = connection.cursor()
     cursor.execute(query)
-
+    # Add and close the connection to SQL
     connection.commit()
     cursor.close()
     connection.close()
 
+# Update your password based on your email, previous password, and new password. Requires old password (self explanatory: for safety reasons)
 def updatePassword(email, oldPassword, newPassword):
     connection = mysql.connector.connect(host = 'localhost', user = 'root', database = 'bankingdata', password = 'haur@b0t')
     query = (f"UPDATE banking SET password='{newPassword}' WHERE email='{email}' AND password='{oldPassword}'")
+    # Create a cursor and execute the command
     cursor = connection.cursor()
     cursor.execute(query)
-
+    # Add and close the connection to SQL
     connection.commit()
     cursor.close()
     connection.close()
 
+# Update your email based on your old email, new email, and password (needed for safety reasons)
 def updateEmail(oldEmail, newEmail, password):
     connection = mysql.connector.connect(host = 'localhost', user = 'root', database = 'bankingdata', password = 'haur@b0t')
     query = (f"UPDATE banking SET email='{newEmail}' WHERE email='{oldEmail}' AND password='{password}'")
+    # Create a cursor and execute the command
     cursor = connection.cursor()
     cursor.execute(query)
-
+    # Add and close the connection to SQL
     connection.commit()
     cursor.close()
     connection.close()
 
+# Add money to your balance based on your email, password, and the amount you want to sign in with
 def addBalance(email, password, deposit):
     connection = mysql.connector.connect(host = 'localhost', user = 'root', database = 'bankingdata', password = 'haur@b0t')
     query = (f"SELECT balance FROM banking WHERE email='{email}' AND password='{password}'")
+    # Create a cursor and execute the command
     cursor = connection.cursor()
     cursor.execute(query)
     result = float(cursor.fetchone()[0])
@@ -50,26 +59,30 @@ def addBalance(email, password, deposit):
 
     query = (f"UPDATE banking SET balance='{newBalance}' WHERE email='{email}' AND password='{password}'")
     cursor.execute(query)
-
+    # Add and close the connection to SQL
     connection.commit()
     cursor.close()
     connection.close()
 
+# Show your balance based on your email and password
 def displayBalance(email, password):
     connection = mysql.connector.connect(host = 'localhost', user = 'root', database = 'bankingdata', password = 'haur@b0t')
     query = (f"SELECT balance FROM banking WHERE email='{email}' AND password='{password}'")
+    # Create a cursor and execute the command
     cursor = connection.cursor()
     cursor.execute(query)
     result = float(cursor.fetchone()[0])
-
+    # Add and close the connection to SQL
     connection.commit()
     cursor.close()
     connection.close()
     print(result)
 
+# Takes money out of your account based on email, password, and how much you want to take out
 def withdrawBalance(email, password, withdrawBalance):
     connection = mysql.connector.connect(host = 'localhost', user = 'root', database = 'bankingdata', password = 'haur@b0t')
     query = (f"SELECT balance FROM banking WHERE email='{email}' AND password='{password}'")
+    # Create a cursor and execute the command
     cursor = connection.cursor()
     cursor.execute(query)
     result = float(cursor.fetchone()[0])
@@ -77,10 +90,18 @@ def withdrawBalance(email, password, withdrawBalance):
 
     query = (f"UPDATE banking SET balance='{newBalance}' WHERE email='{email}' AND password='{password}'")
     cursor.execute(query)
-
+    # Add and close the connection to SQL
     connection.commit()
     cursor.close()
     connection.close()
 
+#Test cases
 if __name__ == "__main__":
-    print("all G")
+    #makeAccount("hamzah@gmail.com", "1234asdf", 99.99)
+    #updatePassword("hamzah@gmail.com", "1234asdf", "properPassword123")
+    #updateEmail("hamzah@gmail.com", "hamzahR@gmail.com", "properPassword123")
+    #addBalance("hamzahR@gmail.com", "properPassword123", 100.99)
+    #withdrawBalance("hamzahR@gmail.com", "properPassword123", 20.99)
+    #displayBalance("hamzahR@gmail.com", "properPassword123")
+    #deleteAccount("hamzahR@gmail.com", "properPassword123")
+    print("All Good!")
